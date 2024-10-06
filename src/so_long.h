beb89dd1d97367ef4f19f5d160cdee99532cd9cd
Zuzana Kutilkova <zkutilko@c3r6s3.42prague.com>
2024-10-06 13:45:04 +0200

    update

--- src/so_long.h
+++ src/so_long.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zkutilko <zkutilko@student.42prague.com    +#+  +:+       +#+        */
+/*   By: zkutilko <zkutilko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 20:08:07 by zkutilko          #+#    #+#             */
-/*   Updated: 2024/10/01 19:50:08 by zkutilko         ###   ########.fr       */
+/*   Updated: 2024/10/06 13:40:26 by zkutilko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,10 +29,45 @@
 # define EXIT					"E"
 # define PLAYER					"P"
 
+# define KEY_W					119
+# define KEY_A					97
+# define KEY_S					115
+# define KEy_D					100
+
+# define KEY_UP					65362
+# define KEY_LEFT				65361
+# define KEY_RIGHT				65363
+# define KEY_DOWN				65364
+
+# define KEY_ESC				65307
+
+# define FRONT					1
+# define LEFT					2
+# define RIGHT					3
+# define DOWN					4
+
 # define WALL_XPM				"../images/wall.xpm"
 # define FLOOR_XPM				"../images/floor.xpm"
 # define COLL_XPM				"../images/coll.xpm"
-# define PLAYER_XPM				"../images/player.xpm"
+# define P_UP_XPM				"../images/up.xpm"
+# define P_LEFT_XPM				"../images/left.xpm"
+# define P_RIGHT_XPM			"../images/right.xpm"
+# define P_DOWN_XPM				"../images/down.xpm"
 # define EXIT_XPM				"../images/exit.xpm"
+
+# define GREEN					"\033[0;32m"
+# define RED					"\033[1;31m"
+# define GREY					"\033[0;90m"
+# define RESET					"\033[0m"
+
+
+
+typedef struct s_game
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	int		movements;
+	int		player;
+} t_game;
 
 #endif
